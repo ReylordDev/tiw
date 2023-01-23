243f18f78b198ae04632107aa55d1efb639993fc
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-01-23 23:57:41 +0100

    word-db fill script added

---
+++ scripts/fill-words.ts
@@ -0,0 +1,54 @@
+import { parse } from "csv-parse";
+import * as path from "path";
+import * as fs from "fs";
+import { prisma } from "../src/server/db";
+
+type Word = {
+  italian: string;
+  english: string;
+  german: string;
+  rank: number;
+};
+
+const doBackfill = () => {
+  const csvFilePath = path.resolve(__dirname, "../words/italian.csv");
+  console.log("filepath", csvFilePath);
+
+  const headers = ["italian", "english", "german", "rank"];
+
+  const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
+
+  parse(
+    fileContent,
+    {
+      delimiter: ",",
+      columns: headers,
+      fromLine: 2,
+      skip_empty_lines: true,
+      cast: (value, context) => {
+        if (context.column === "rank") {
+          return parseInt(value, 10);
+        }
+        return value;
+      },
+    },
+    (err, result: Word[]) => {
+      if (err) {
+        console.error(err);
+      }
+      result.map(async (word) => {
+        console.log(word);
+        await prisma.word.create({
+          data: {
+            italian: word.italian,
+            english: word.english,
+            german: word.german,
+            rank: word.rank,
+          },
+        });
+      });
+    }
+  );
+};
+
+doBackfill();
