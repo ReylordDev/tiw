461e7a38437c837299fba3e7a12f0779acff929b
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-01-23 23:58:14 +0100

    commented out env import because of db fill script

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "../env/server.mjs";
-
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
@@ -11,9 +9,13 @@ export const prisma =
   global.prisma ||
   new PrismaClient({
     log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      // env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      ["query", "error", "warn"],
   });
 
-if (env.NODE_ENV !== "production") {
+// if (env.NODE_ENV !== "production") {
+//   global.prisma = prisma;
+// }
+if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
