fe48954dbb0e4a5f28cf52401c07590950f9a6bb
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-01-26 19:51:28 +0100

    organize imports

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
   getById: protectedProcedure
