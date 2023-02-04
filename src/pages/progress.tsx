2801a911cc30bf3acc8a927ef580819cd1e325c0
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-02-04 20:21:05 +0100

    progress page divs struggle

--- src/pages/progress.tsx
+++ src/pages/progress.tsx
@@ -2,6 +2,8 @@ import { useSession } from "next-auth/react";
 import Head from "next/head";
 import type { NextPage } from "next/types";
 import { api } from "../utils/api";
+import Image from "next/image"
+import Link from "next/link";
 
 const Home: NextPage = () => {
     const { data: session } = useSession();
@@ -29,6 +31,21 @@ function ProgressPage({ userId }: { userId: string }) {
         id: userId,
     })
     console.log(user)
-    return <main className="min-h-screen flex flex-col items-center font-['Virgil'] bg-[#121212] justify-between">
+    return <main className="min-h-screen flex flex-col font-['Virgil'] bg-[#121212] justify-between">
+        <div className="flex w-full py-4 px-4 lg:px-24 bg-red-400 justify-between">
+            <LanguageSelectionButton />
+            <div className="text-5xl lg:text-8xl lg:border-7 border-b-4 px-4 py-4    ">Progress</div>
+            <div className=" bg-green-400">
+                <Link href="menu" className=" lg:border-4 lg:px-6 rounded-2xl text-4xl  ">
+                    Back
+                </Link>
+            </div>
+        </div>
     </main>
+}
+
+function LanguageSelectionButton() {
+    return <div className="bg-blue-400">
+        <Image src={"globe.svg"} height={48} width={48} alt="Globe for language selection" />
+    </div>;
 }
