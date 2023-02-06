c8442bd5c6b84d4257f183ca5ac99daf36827526
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-02-06 16:14:34 +0100

    custom Head component

---
+++ src/pages/components/myHead.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function MyHead() {
+    return (
+        <Head>
+            <title>Top Italian Words</title>
+            <meta name="description" content="Learn the 1000 most common italian words with a spaced repetition algorithm." />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
