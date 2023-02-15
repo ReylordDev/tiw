209005015ed8fcb50c1d84da4c68986260fa0cb8
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-02-15 21:56:29 +0100

    next-intl init

---
+++ global.d.ts
@@ -0,0 +1,3 @@
+// Declaring this interface provides type safety for message keys
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+declare type IntlMessages = typeof import("./locales/en.json");
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "../utils/api";
+import type { AbstractIntlMessages } from "next-intl";
+import { NextIntlProvider } from "next-intl";
 
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{
+  session: Session | null;
+  messages: AbstractIntlMessages;
+}> = ({ Component, pageProps }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <NextIntlProvider messages={pageProps.messages}>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </NextIntlProvider>
   );
 };
 
